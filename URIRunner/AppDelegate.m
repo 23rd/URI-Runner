a4a6018032aac97a0914f1f82ea365f023c5ddd7
23rd <23rd@vivaldi.net>
2022-06-01 11:28:39 +0300

    Changed code style.

--- URIRunner/AppDelegate.m
+++ URIRunner/AppDelegate.m
@@ -21,42 +21,50 @@
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    [[NSAppleEventManager sharedAppleEventManager] setEventHandler:self
-                                                       andSelector:@selector(handleURLEvent:withReplyEvent:)
-                                                     forEventClass:kInternetEventClass
-                                                        andEventID:kAEGetURL];
+- (void) applicationDidFinishLaunching:(NSNotification *)aNotification {
+	[[NSAppleEventManager sharedAppleEventManager]
+		setEventHandler:self
+		andSelector:@selector(handleURLEvent:withReplyEvent:)
+		forEventClass:kInternetEventClass
+		andEventID:kAEGetURL];
 }
 
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+- (void) applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
 }
 
-- (void)handleURLEvent:(NSAppleEventDescriptor*)event
-        withReplyEvent:(NSAppleEventDescriptor*)replyEvent
-{
-    NSString *url = [event paramDescriptorForKeyword:keyDirectObject].stringValue;
-    
-    if ([url hasPrefix:AUTOMATOR_URL_PREFIX]) {
-        // TODO: execute Automator workflow
-    } else if ([url hasPrefix:APPLESCRIPT_URL_PREFIX]) {
-        [self.statusProgressIndicator setIndeterminate:YES];
-        [self.statusProgressIndicator startAnimation:self];
-        [self.statusTextField setStringValue:NSLocalizedString(@"Running AppleScript", nil)];
-        
-        NSString *source = [[url stringByReplacingCharactersInRange:NSMakeRange(0, APPLESCRIPT_URL_PREFIX.length)
-                                                         withString:@""] stringByRemovingPercentEncoding];
-        NSAppleScript *appleScript = [[NSAppleScript alloc] initWithSource:source];
-        
-        NSDictionary *error;
-        [appleScript executeAndReturnError:&error];
-        // TODO: error handling
-        
-        [self.statusProgressIndicator setIndeterminate:NO];
-        [self.statusProgressIndicator stopAnimation:self];
-        [self.statusTextField setStringValue:NSLocalizedString(@"Idle", nil)];
-    }
+- (void) handleURLEvent:(NSAppleEventDescriptor*)event
+		withReplyEvent:(NSAppleEventDescriptor*)replyEvent {
+	NSString *url = [event
+		paramDescriptorForKeyword:keyDirectObject].stringValue;
+
+	if ([url hasPrefix:AUTOMATOR_URL_PREFIX]) {
+		// TODO: execute Automator workflow
+	} else if ([url hasPrefix:APPLESCRIPT_URL_PREFIX]) {
+		[self.statusProgressIndicator setIndeterminate:YES];
+		[self.statusProgressIndicator startAnimation:self];
+		[self.statusTextField
+			setStringValue:NSLocalizedString(@"Running AppleScript", nil)];
+
+		NSString *source = [
+				[url
+					stringByReplacingCharactersInRange:NSMakeRange(
+						0,
+						APPLESCRIPT_URL_PREFIX.length)
+					withString:@""]
+			stringByRemovingPercentEncoding];
+		NSAppleScript *appleScript = [[NSAppleScript alloc]
+			initWithSource:source];
+
+		NSDictionary *error;
+		[appleScript executeAndReturnError:&error];
+		// TODO: error handling
+
+		[self.statusProgressIndicator setIndeterminate:NO];
+		[self.statusProgressIndicator stopAnimation:self];
+		[self.statusTextField setStringValue:NSLocalizedString(@"Idle", nil)];
+	}
 }
 
 @end
